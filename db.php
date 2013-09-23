ca7df1cab6e2b359d2b760ca241a704d12705109
Pasindu De Silva <ghosteyes17@ymail.com>
2013-09-23 16:41:39 +0530

    added session tbl sql

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php  
 	mysql_connect("localhost", "root", "" )or die(mysql_error()); 
-	mysql_select_db("celebrity") or die(mysql_error()); 
+	mysql_select_db("sessiondb") or die(mysql_error()); 
 ?>
 
 
---
+++ sessions.sql
@@ -0,0 +1,28 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.25a - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             7.0.0.4389
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table celebrity.sessions
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `sessionsid` varchar(100) NOT NULL,
+  `tel` varchar(50) DEFAULT NULL,
+  `menu` varchar(50) DEFAULT NULL,
+  `pg` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `others` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`sessionsid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Handle the sessions';
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
